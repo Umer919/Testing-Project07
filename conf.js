dc084ad35ff6fee5331e170cde3f0acc200250b6
Umer Ziyad Satti <umersatti919@gmail.com>
2020-05-28 23:38:33 +0500

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,26 @@
+
+
+
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['tests/example_spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+
+}
+};
+  
+
